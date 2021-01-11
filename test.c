2a62fc33656da83817551b6152879c782e3b7144
tw_yshuang <rober5566a@gmail.com>
2021-01-12 03:06:59 +0800

    [mod][rm] mod: test.c, this version can only execute test.c to complete brightness controll~~~ ver.0.1 . rm: brightness_controll.sh, useless.

--- test.c
+++ test.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/inotify.h>
 
 #include <stdlib.h>
+
+double getXrandrBrightness()
+{
+    FILE *fp;
+    double value;
+    char command_buffer[8];
+    fp = popen("xrandr --verbose --current | grep -i Brightness | tail -c 5", "r");
+    fgets(command_buffer, sizeof(command_buffer), fp);
+    // printf("%s", command_buffer);
+    pclose(fp);
+    sscanf(command_buffer, "%lf", &value);
+    // printf("%lf", value);
+
+    return value;
+}
+
 int read_value(char *FILENAME)
 {
     char c_value[8];
@@ -24,18 +41,44 @@ int read_value(char *FILENAME)
     return value;
 }
 
+int getWordLenght(char *wordArray[], int size_of_array)
+{
+    int item_of_array = size_of_array / sizeof(&wordArray);
+    int wordLenght = 0;
+    for (int i = 0; i < item_of_array; ++i)
+    {
+        wordLenght += strlen(wordArray[i]);
+    }
+    return ++wordLenght;
+}
+
+void getConcatedWord(char *command_buffer, char *wordArray[], int size_of_array, int wordLenght)
+{
+    int item_of_array = size_of_array / sizeof(&wordArray);
+    char concatedWord[wordLenght];
+    memset(concatedWord, '\0', wordLenght);
+    for (int i = 0; i < item_of_array; ++i)
+    {
+        strcat(concatedWord, wordArray[i]);
+    }
+    // printf("%d\n", wordLenght);
+    printf("%s\n", concatedWord);
+    strcpy(command_buffer, concatedWord);
+}
+
 double main(int argc, char *argv[])
 {
-    int fd;
-    int wd;
+    int fd, wd;
     char *FILE_NAME1 = argv[1];
     char *FILE_NAME2 = argv[2];
-    int target_value, max_value;
-    double target_rate;
+    char Monitor[] = "eDP-1-1";
 
     struct inotify_event *event;
     // char buf[BUFSIZ];
     char buf[128];
+
+    int target_value, max_value;
+    double current_rate, target_rate;
 
     fd = inotify_init();
     if (fd < 0)
@@ -49,15 +92,27 @@ double main(int argc, char *argv[])
         perror("inotify_add-watch");
     }
 
-    while (read(fd, buf, sizeof(buf) - 1) > 0)
+    do
     {
+        current_rate = getXrandrBrightness();
         target_value = read_value(FILE_NAME1);
         max_value = read_value(FILE_NAME2);
         target_rate = (double)target_value / max_value;
         // printf("%d\n%d\n%f\n", target_value, max_value, target_rate);
-        return fprintf(stdout, "%f", target_rate);
+        // return fprintf(stdout, "%f", target_rate);
         // return target_rate;
-    }
+
+        char command_rate[16];
+        sprintf(command_rate, "%f", target_rate); // Convert double to char
+        char *command[] = {"xrandr --output ", Monitor, " --brightness ", command_rate};
+        int command_len = getWordLenght(command, sizeof(command));
+        char *command_buffer = malloc(command_len);
+        getConcatedWord(command_buffer, command, sizeof(command), command_len);
+        printf("%s\n", command_buffer);
+        pclose(popen(command_buffer, "r"));
+        free(command_buffer);
+
+    } while (read(fd, buf, sizeof(buf) - 1) > 0);
 
     return 0;
 }
