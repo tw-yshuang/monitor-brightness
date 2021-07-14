3a282b64e9c3a644eb8c1497f9ee0981eb5002e9
Y.S. Huang <tw.yshuang@gmail.com>
2021-07-14 14:47:40 +0800

    Revert "Install script for convenience (#2)"
    This reverts commit 4e29c2f6ff4f8b1d37802013f0f791395c74d168.

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-sudo apt update
-sudo apt -y install x11-xserver-utils
-sudo apt -y install make build-essential
-sudo make
-monitor-brightness $(xrandr | grep ' connected ' | awk '{ print$1 }') &
